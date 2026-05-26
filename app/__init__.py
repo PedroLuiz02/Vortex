2ae0c7d5c2ed28e00c198960311ee6e09af87d3b
PedroLuiz02 <pedroluiz0034@gmail.com>
2026-05-26 16:51:47 -0300

    Config Render

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
