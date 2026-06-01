84a3b5629c095d6f8251829cbef295d2194804d0
PedroLuiz02 <pedroluiz0034@gmail.com>
2026-06-01 15:59:11 -0300

    Configuração Supabase

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,14 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL","sqlite:///database.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = (os.getenv("DATABASE_URL") + "?sslmode=require")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
+print(os.getenv("DATABASE_URL"))
+
 from . import routes
+
+with app.app_context():
+    db.create_all()
